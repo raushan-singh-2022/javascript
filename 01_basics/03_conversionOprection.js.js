df46c86b389ae381a5094d78d45ee79547f72381
Raushan Kumar <104673651+raushan-singh-2022@users.noreply.github.com>
2026-05-30 19:02:38 +0000

    conversion

--- 01_basics/03_conversionOprection.js.js
+++ 01_basics/03_conversionOprection.js.js
@@ -12,10 +12,43 @@ let valueInNumber = Number(score);
 
 let isLoggedIn=""
 let boolenIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn)
+//console.log(boolenIsLoggedIn)
 
 let sumNumber = 55
 let stringNumber = String(sumNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+//************* Oprections *************
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+//console.log(2/3)
+// console.log(2%3)
+
+//**** Srting Add 
+// let str1 = "Hello"
+// let str2 = " Raushan"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+let num = 1
+let num2 = "3"
+let num3 = num + num2
+
+//console.log(num3)
+
+// console.log("1" + 2) //if first use string then all string
+// console.log(1 + "2")//
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")//if value in string then fist oprection then last add string
