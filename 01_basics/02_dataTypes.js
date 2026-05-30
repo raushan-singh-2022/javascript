dc36fce7a068f634022c6116c878a3c0ea7095c5
Raushan Kumar <104673651+raushan-singh-2022@users.noreply.github.com>
2026-05-30 17:49:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js as never version
+
+//alert(3+3) we are useing node js not browser
+
+let name="raushan"
+let age=20
+let state;
+
+//number=> 2 to power 53
+//bigint=>
+// string=>""
+//boolen => true/false
+//null=> standlone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined) //undefine
+console.log(typeof null) //object
