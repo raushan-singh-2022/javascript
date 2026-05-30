4b7d85835a404b53d4e87493650a9fc3f847c42e
Raushan Kumar <104673651+raushan-singh-2022@users.noreply.github.com>
2026-05-30 18:18:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOprection.js.js
@@ -0,0 +1,21 @@
+let score="33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" =>33
+//"33abc"=>Nan
+//true=1 and false=0
+
+let isLoggedIn=""
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn)
+
+let sumNumber = 55
+let stringNumber = String(sumNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
